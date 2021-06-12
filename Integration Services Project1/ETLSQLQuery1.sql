b6654618eaf0e55eaadc1595b209bcc45b252e8d
Shivani Patel <75600348+shivani-patel-coder@users.noreply.github.com>
2021-06-12 16:05:27 -0700

    Add files via upload

---
+++ Integration Services Project1/ETLSQLQuery1.sql
@@ -0,0 +1,17 @@
+USE AdventureWorks
+GO
+--Create new table to hold performance targets
+--CREATE TABLE RepPerformanceTargets
+--(
+	--ID INT PRIMARY KEY IDENTITY(1,1),
+	--FirstName NVARCHAR(255),
+	--LastName NVARCHAR(255),
+	--TargetSalesQuantity INT,
+	--TargetSalesRevenue MONEY,
+	--TargetYear INT
+--)
+
+--TRUNCATE TABLE RepPerformanceTargets
+
+SELECT * FROM RepPerformanceTargets
+
